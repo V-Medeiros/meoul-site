b16dfe8484a3be918a4d6281fee957e2cd65f011
Victor <victorme2806@gmail.com>
2026-08-05 21:00:54 -0300

    janela

--- meu/src/components/janela/main.tsx
+++ meu/src/components/janela/main.tsx
@@ -4,16 +4,18 @@ import styles from "./style.module.css";
 type JanelaProps = {
   top: number;
   left: number;
+  width: number;
+  height: number;
 };
 
-export function Janela({ top, left }: JanelaProps) {
+export function Janela({ top, left, width, height }: JanelaProps) {
   const { posicao, elementoRef, eventosDeArraste } = useArraste({ top, left });
 
   return (
     <div
       ref={elementoRef}
       className={styles.stage}
-      style={{ top: posicao.top, left: posicao.left }}
+      style={{ top: posicao.top, left: posicao.left, width, height }}
     >
       <div className={styles.titleBar} {...eventosDeArraste}>
         Janela
