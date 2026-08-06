90f9ec01e7211e07516d4b474a48a6055808c894
Victor <victorme2806@gmail.com>
2026-08-06 19:42:40 -0300

    fix : layout

--- meu/src/components/templateAtalhos/main.tsx
+++ meu/src/components/templateAtalhos/main.tsx
@@ -3,7 +3,7 @@ import { Atalho } from "../atalho/main";
 export function TemplateAtalhos() {
   return (
     <>
-      <Atalho nome="Sobre" top={140} left={200} width={480} height={320} />
+      <Atalho nome="Sobre mim" top={140} left={200} width={480} height={320} />
       <Atalho nome="Projetos" top={600} left={1600} width={360} height={480} />
       <Atalho nome="Experiência" top={150} left={1500} width={600} height={400} />
       <Atalho nome="Habilidades" top={500} left={890} width={420} height={280} />
