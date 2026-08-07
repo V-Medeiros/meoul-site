6f457049ac610f88a6bdc615f06de47b66e64d3a
gavianderson <victorme2806@gmail.com>
2026-08-07 17:45:23 -0300

    m

--- meu/src/components/templateAtalhos/main.tsx
+++ meu/src/components/templateAtalhos/main.tsx
@@ -3,14 +3,16 @@ import { Atalho } from "../atalho/main";
 export function TemplateAtalhos() {
   return (
     <>
-      <Atalho nome="Sobre mim" top={140} left={200} width={480} height={320} 
-      descricao="descrição" />
-      
+      <Atalho nome="Sobre mim" top={140} left={200} width={480} height={320}
+        descricao="alou" />
       <Atalho nome="Projetos" top={600} left={1600} width={360} height={480}
-       descricao="alou"/>
-      <Atalho nome="Experiência" top={150} left={1500} width={600} height={400} descricao="alou"/>
-      <Atalho nome="Habilidades" top={500} left={890} width={420} height={280} descricao="alou"/>
-      <Atalho nome="Curriculo" top={70} left={700} width={640} height={440} descricao="alou"/>
+        descricao="alou" />
+      <Atalho nome="Experiência" top={150} left={1500} width={600} height={400}
+        descricao="alou" />
+      <Atalho nome="Habilidades" top={500} left={890} width={420} height={280}
+        descricao="alou" />
+      <Atalho nome="Curriculo" top={70} left={700} width={640} height={440}
+        descricao="alou" />
     </>
   );
 }
