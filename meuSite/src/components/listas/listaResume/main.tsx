d0f17a3234ec2e74a81fbb3692c5baa6e660bd56
Victor <victorme2806@gmail.com>
2026-08-11 23:47:47 -0300

    feat: aplica dados ao resume

--- meuSite/src/components/listas/listaResume/main.tsx
+++ meuSite/src/components/listas/listaResume/main.tsx
@@ -1,47 +1,11 @@
-import type { Projeto } from "../../../data/portifolio";
-import styles from "./style.module.css";
-
-type ListaProjetosProps = {
-  projetos: Projeto[];
+type ListaResumeProps = {
+  resume: string;
 };
 
-export function ListaProjetos({ projetos }: ListaProjetosProps) {
+export function ListaResume({ resume }: ListaResumeProps) {
   return (
-    <div className={styles.lista}>
-      {projetos.map((projeto) => (
-        <article className={styles.projeto} key={projeto.nome}>
-          <h2>{projeto.nome}</h2>
-          <p>{projeto.descricao}</p>
-
-          <ul className={styles.tecnologias}>
-            {projeto.tecnologias.map((tecnologia) => (
-              <li key={tecnologia}>{tecnologia}</li>
-            ))}
-          </ul>
-
-          <div className={styles.links}>
-            {projeto.repositorio && (
-              <a
-                href={projeto.repositorio}
-                target="_blank"
-                rel="noreferrer"
-              >
-                Repositório
-              </a>
-            )}
-
-            {projeto.demonstracao && (
-              <a
-                href={projeto.demonstracao}
-                target="_blank"
-                rel="noreferrer"
-              >
-                Demonstração
-              </a>
-            )}
-          </div>
-        </article>
-      ))}
+    <div>
+      {resume}
     </div>
   );
 }
