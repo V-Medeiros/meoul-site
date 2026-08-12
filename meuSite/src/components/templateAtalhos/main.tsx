0faee982b4f9ecfdcc0dfb583082c8124e3f282c
Victor <victorme2806@gmail.com>
2026-08-11 23:47:53 -0300

    feat: conecta secoes do portfolio aos atalhos

--- meuSite/src/components/templateAtalhos/main.tsx
+++ meuSite/src/components/templateAtalhos/main.tsx
@@ -1,20 +1,57 @@
 import { Atalho } from "../atalho/main";
 import { ListaProjetos } from "../listaProjetos/main";
 import { portfolio } from "../../data/portifolio";
+import { ListaResume } from "../listas/listaResume/main";
+import { ListaTools } from "../listas/listaTools/main";
 
 export function TemplateAtalhos() {
   return (
     <>
-      <Atalho id="sobre" nome="about" top={150} left={1500} width={600} height={400} 
-        descricao= "about me"/>
-      <Atalho id="projetos" nome="Projects" top={600} left={1600} width={360} height={680}
-        descricao={<ListaProjetos projetos={portfolio.projetos} />} />
-      <Atalho id="experiencia" nome="Resume" top={70} left={700} width={740} height={440}
-        descricao="alou" />
-      <Atalho id="habilidades" nome="Tools"  top={140} left={200} width={480} height={320}
-        descricao="alou" />
-      <Atalho id="curriculo" nome="Contact"   top={500} left={890} width={420} height={280}
-        descricao="alou" />
+      <Atalho
+        id="about"
+        nome="about"
+        top={150}
+        left={1500}
+        width={600}
+        height={400}
+        descricao="about me"
+      />
+      <Atalho
+        id="projects"
+        nome="Projects"
+        top={600}
+        left={1600}
+        width={360}
+        height={680}
+        descricao={<ListaProjetos projetos={portfolio.projects} />}
+      />
+      <Atalho
+        id="contact"
+        nome="Contact"
+        top={500}
+        left={890}
+        width={420}
+        height={280}
+        descricao="alou"
+      />
+      <Atalho
+        id="tools"
+        nome="Tools"
+        top={140}
+        left={200}
+        width={480}
+        height={320}
+        descricao={<ListaTools tools={portfolio.tools} />}
+      />
+      <Atalho
+        id="resume"
+        nome="Resume"
+        top={70}
+        left={700}
+        width={740}
+        height={440}
+        descricao={<ListaResume resume={portfolio.resume} />} 
+      />
     </>
   );
 }
